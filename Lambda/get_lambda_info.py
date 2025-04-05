0986c8dfeb5acdae6d4a3151bbb84d7eaf2cb5aa
SamsonX <lx223809@hotmail.com>
2025-04-05 13:38:27 +0800

    feat[get_lambda_info]: get lambda concurrency setting parallely, format last deployed time

--- Lambda/get_lambda_info.py
+++ Lambda/get_lambda_info.py
@@ -1,11 +1,14 @@
 import json
+import multiprocessing
 import os
 import string
 import sys
 from dataclasses import dataclass, fields
+from datetime import datetime
 from typing import List, ClassVar, Dict
 
 import boto3
+import boto3.session
 from botocore.client import Config as BotoConfig
 from botocore.exceptions import ClientError
 
@@ -17,6 +20,7 @@ sys.path.append(os.getcwd())
 from utils.aws_client_error_handler import handle_expired_token_exception
 from utils.aws_client_helper import get_aws_profile
 from utils.aws_consts import AllEnvs, Env
+from utils.SystemTools.file_system_helper import create_dir_if_not_exists
 from utils.TablePrinter.table_printer import BaseTable, BaseRow, ColumnConfig, ColumnAlignment, CondFmtExactMatch
 
 # region 配置项
@@ -30,17 +34,6 @@ REGIONS = [
     # 'us-east-1',
 ]
 # endregion 配置项
-
-if sys.platform == 'win32':
-    # powershell 不支持
-    LAMBDA_NAME_HREF = False
-elif sys.platform == 'linux':
-    LAMBDA_NAME_HREF = True
-elif sys.platform == 'darwin':
-    # macOS 默认 Terminal 不支持
-    LAMBDA_NAME_HREF = False
-else:
-    raise RuntimeError("Unsupported platform")
 
 OUTPUT_DIR = f'./{CURR_FOLDER_NAME}/Data/output'
 
@@ -80,16 +73,20 @@ class Function:
 class FunctionRow(BaseRow):
     FunctionName: str = 'NA'
     __FunctionName_config: ClassVar[ColumnConfig] = ColumnConfig(align=ColumnAlignment.LEFT)
+    FunctionName_href: str = 'NA'
     Region: str = 'NA'
     __Region_config: ClassVar[ColumnConfig] = ColumnConfig(alias='地区')
     Timeout: int = None
     __Timeout_config: ClassVar[ColumnConfig] = ColumnConfig(alias='超时')
     MemorySize: int = None
-    Throttled: bool = 'NA'
+    __MemorySize_config: ClassVar[ColumnConfig] = ColumnConfig(alias='内存')
+    CurrencySetting: str = 'NA'
+    __CurrencySetting_config: ClassVar[ColumnConfig] = ColumnConfig(
+        alias='并发设置', conditional_format=CondFmtExactMatch(match_target='Throttled')
+    )
     __Throttled_config: ClassVar[ColumnConfig] = ColumnConfig(conditional_format=CondFmtExactMatch(match_target=True))
-    LastModified: str = 'NA'
-    Url: str = 'NA'
-    __Url_config: ClassVar[ColumnConfig] = ColumnConfig(align=ColumnAlignment.LEFT, hide=LAMBDA_NAME_HREF)
+    LastDeployDt: str = 'NA'
+    __LastDeployDt_config: ClassVar[ColumnConfig] = ColumnConfig(alias='最后部署时间')
 
 
 class FunctionTable(BaseTable):
@@ -106,12 +103,12 @@ def get_env_rgn_functions(env: Env, region: str) -> List[dict]:
         param = {'Marker': marker} if marker else dict()
 
         try:
-            resp = client.list_functions(**param)
+            resp: dict = client.list_functions(**param)
         except ClientError as e:
-            if 'security token' in e.response['Error']['Message']\
-                    and 'invalid' in e.response['Error']['Message']:
-                print(f'Exception: {e.__dict__}')
+            if 'ExpiredTokenException' in e.response['Error']['Code']:
                 handle_expired_token_exception(session)
+            else:
+                print(f'Exception: {e.__dict__}')
             break
         if resp.get('ResponseMetadata', {}).get('HTTPStatusCode', 0) != 200:
             raise Exception(f'non 200 code: {resp}')
@@ -137,6 +134,21 @@ def get_functions_currency(env: Env, region: str, function_names: List[str]) -> 
     return ret
 
 
+def get_functions_currency_worker(
+        env: Env, region: str, function_name: str, shared_dict: dict, stop_event
+):
+    session = boto3.session.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
+    client = session.client('lambda', config=BotoConfig(connect_timeout=3, retries={"mode": "standard"}))
+    while not stop_event.is_set():
+        try:
+            resp = client.get_function_concurrency(FunctionName=function_name)
+            shared_dict[function_name] = resp
+            return
+        except ClientError as e:
+            print(f'Exception: {e.__dict__}')
+            return
+
+
 # region 保存、读取 json 文件
 def __get_json_path(file_name: str) -> str:
     return f'{OUTPUT_DIR}/{file_name}' + ('' if file_name.endswith('.json') else '.json')
@@ -160,6 +172,7 @@ def read_list_of_dict_from_json(file_name: str) -> List[dict]:
 
 def save_dict_of_dict_to_json(file_name: str, dict_of_dict: Dict[str, dict]):
     file_path = __get_json_path(file_name)
+    create_dir_if_not_exists(file_path=file_path)
     print(f'Save File: {file_path}')
     with open(file_path, 'w', encoding='utf8') as f_out:
         f_out.writelines(json.dumps(dict_of_dict, indent=4))
@@ -168,6 +181,7 @@ def save_dict_of_dict_to_json(file_name: str, dict_of_dict: Dict[str, dict]):
 
 def save_list_of_dict_to_json(file_name: str, list_of_dict: List[dict]):
     file_path = __get_json_path(file_name)
+    create_dir_if_not_exists(file_path=file_path)
     print(f'Save File: {file_path}')
     with open(file_path, 'w', encoding='utf8') as f_out:
         f_out.writelines(json.dumps(list_of_dict, indent=4))
@@ -203,25 +217,21 @@ def main():
     table = FunctionTable()
     for rgn in REGIONS:
         file_name_functions = f'{"prod" if ENV.is_prod_aws else "dev"}_{rgn}_Functions.json'
-        file_name_functions_ccy = f'{"prod" if ENV.is_prod_aws else "dev"}_{rgn}_FunctionsCcy.json'
 
         rgn_funcs = get_env_rgn_functions(env=ENV, region=rgn)
         save_list_of_dict_to_json(file_name_functions, rgn_funcs)
         rgn_funcs = read_list_of_dict_from_json(file_name_functions)
         fn_all += parse_list_functions_resp(rgn_funcs)
 
-        func_ccy = get_functions_currency(env=ENV, region=rgn, function_names=[fn.FunctionName for fn in fn_all])
-        save_dict_of_dict_to_json(file_name_functions_ccy, func_ccy)
-        func_ccy = read_dict_of_dict_from_json(file_name_functions_ccy)
-        fn_ccy_all = {**fn_ccy_all, **func_ccy}
-
+    # 过滤出当前环境的函数
+    fn_curr_env_rgn: List[Function] = []
     for fn in fn_all:
         is_env_main, is_env_sub = False, False
         if not fn.FunctionName.startswith(ENV.name):
             continue
         if fn.FunctionName.endswith('FnStateChange'):
             continue
-        if fn.FunctionName.startswith(f'{ENV.name}--'):
+        if fn.FunctionName.startswith(f'{ENV.name}--{SUB_ENV}'):
             # 子环境方法
             is_env_sub = True
         elif fn.FunctionName.replace(f'{ENV.name}-', '')[0] in string.ascii_uppercase:
@@ -230,22 +240,50 @@ def main():
         # 不是当前环境的方法
         if not (is_env_main or is_env_sub):
             continue
+        fn_curr_env_rgn.append(fn)
 
+    # 获取当前环境函数并发设置
+    fn_ccy_all: dict = {}
+    with multiprocessing.Manager() as manager:
+        stop_event = multiprocessing.Event()
+        shared_dict = manager.dict()
+        processes_func_ccy = []
+        for fn in fn_curr_env_rgn:
+            process = multiprocessing.Process(
+                target=get_functions_currency_worker, args=(ENV, rgn, fn.FunctionName, shared_dict, stop_event)
+            )
+            processes_func_ccy.append(process)
+            process.start()
+        try:
+            for process in processes_func_ccy:
+                process.join()
+            fn_ccy_all = dict(shared_dict)
+        except KeyboardInterrupt as e:
+            stop_event.set()
+            for process in processes_func_ccy:
+                process.join()
+            # 重新抛出异常
+            raise e
+
+    # 输出函数表格
+    for fn in fn_curr_env_rgn:
         fn_rgn = fn.FunctionArn.split(':lambda:')[1].split(':')[0]
-        is_throttled = fn_ccy_all.get(fn.FunctionName, {}).get('ReservedConcurrentExecutions', -1) == 0
-        if LAMBDA_NAME_HREF:
-            func_name = f"\033]8;;{get_lambda_url(fn_rgn, fn.FunctionName)}\033\\{fn.FunctionName}\033]8;;\033\\"
-            func_name = fn.FunctionName
+        fn_ccy = fn_ccy_all.get(fn.FunctionName, {})
+        if 'ReservedConcurrentExecutions' in fn_ccy:
+            reserved_ccy = fn_ccy['ReservedConcurrentExecutions']
+            ccy_setting = 'Throttled' if reserved_ccy == 0 else f'{reserved_ccy}'
         else:
-            func_name = fn.FunctionName
+            ccy_setting = '非预留账户并发'
+        reserved_ccy = fn_ccy_all.get(fn.FunctionName, {}).get('ReservedConcurrentExecutions', -1)
+        last_modified_dt = datetime.strptime(fn.LastModified, '%Y-%m-%dT%H:%M:%S.%f%z')
         table.insert_row(FunctionRow(
-            FunctionName=func_name,
+            FunctionName=fn.FunctionName,
             Region=fn_rgn,
             Timeout=fn.Timeout,
             MemorySize=fn.MemorySize,
-            Throttled=is_throttled,
-            LastModified=fn.LastModified,
-            Url=get_lambda_url(fn_rgn, fn.FunctionName),
+            CurrencySetting=ccy_setting,
+            LastDeployDt=datetime.strftime(last_modified_dt, '%Y-%m-%d %H:%M %z')[:-2],
+            FunctionName_href=get_lambda_url(fn_rgn, fn.FunctionName),
         ))
     table.print_table(order_by=['FunctionName', 'Region'])
 

2e24bd5b79de309296ff1bd4f938ffa614a6f769
SamsonX <lx223809@hotmail.com>
2025-07-05 12:25:11 +0800

    feat[get_lambda_info]: add get_env_rgn_functions_worker for parallelization

--- Lambda/get_lambda_info.py
+++ Lambda/get_lambda_info.py
@@ -96,7 +96,7 @@ class FunctionTable(BaseTable):
 
 
 def get_env_rgn_functions(env: Env, region: str) -> List[dict]:
-    session = boto3.session.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
+    session = boto3.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
     client = session.client('lambda', config=BotoConfig(connect_timeout=3, retries={"mode": "standard"}))
 
     functions = []
@@ -126,8 +126,61 @@ def get_env_rgn_functions(env: Env, region: str) -> List[dict]:
     return functions
 
 
+def get_env_rgn_functions_worker(
+        env: Env, region: str,
+        shared_dict_rgn_fn: Dict[str, Dict[str, dict]],
+        stop_event, manager
+):
+    """
+    Args:
+        env (Env): 环境，用于区分生产和开发环境
+        region (str): lambda 地区
+        shared_dict_rgn_fn (dict[str, dict[str, dict]]): dict[region, dict[fn_name, fn_info]]
+        stop_event (multiprocessing.Event): 中断当前任务
+        manager (_type_): 用于创建共享字典里的子共享字典
+
+    Raises:
+        Exception: _description_
+    """
+    print(f'Processing functions in region: {region}')
+    session = boto3.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
+    client = session.client('lambda', config=BotoConfig(connect_timeout=3, retries={"mode": "standard"}))
+
+    functions = []
+    marker = None
+    while True and not stop_event.is_set():
+        param = {'Marker': marker} if marker else dict()
+
+        try:
+            resp: dict = client.list_functions(**param)
+        except ClientError as e:
+            if 'ExpiredTokenException' in e.response['Error']['Code']:
+                handle_expired_token_exception(session)
+                sys.exit(0)
+            else:
+                print(f'Exception: {e.__dict__}')
+            break
+        if resp.get('ResponseMetadata', {}).get('HTTPStatusCode', 0) != 200:
+            raise Exception(f'non 200 code: {resp}')
+        marker = resp.get('NextMarker', '')
+        functions += resp.get('Functions', [])
+
+        if region not in shared_dict_rgn_fn:
+            shared_dict_rgn_fn[region] = manager.dict()
+        for fn in functions:
+            # 检查 aws 是否改了接口返回
+            if 'FunctionName' not in fn:
+                print('[Warning] resp missing key: "FunctionName"')
+                continue
+            shared_dict_rgn_fn[region][fn['FunctionName']] = fn
+
+        if not marker:
+            break
+    print(f'Get {len(functions)} functions in region: {region}')
+
+
 def get_functions_currency(env: Env, region: str, function_names: List[str]) -> Dict[str, dict]:
-    session = boto3.session.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
+    session = boto3.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
     client = session.client('lambda', config=BotoConfig(connect_timeout=3, retries={"mode": "standard"}))
 
     ret = {}
@@ -140,7 +193,7 @@ def get_functions_currency(env: Env, region: str, function_names: List[str]) -> 
 def get_functions_currency_worker(
         env: Env, region: str, function_name: str, shared_dict: dict, stop_event
 ):
-    session = boto3.session.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
+    session = boto3.Session(region_name=region, profile_name=get_aws_profile(region, env.is_prod_aws))
     client = session.client('lambda', config=BotoConfig(connect_timeout=3, retries={"mode": "standard"}))
     while not stop_event.is_set():
         try:
@@ -216,13 +269,114 @@ def get_lambda_url(region: str, function_name):
     return fn_url
 
 
+def main_parallel():
+    fn_ccy_all: Dict = {}
+    table = FunctionTable()
+
+    # 获取所有地区的函数信息
+    rgn_fn_all: Dict[str, Dict[str, dict]] = {}
+    with multiprocessing.Manager() as manager:
+        stop_event = multiprocessing.Event()
+        shared_dict_env_rgn_fn = manager.dict()
+        processes_get_fn = []
+        for rgn in REGIONS:
+            process = multiprocessing.Process(
+                target=get_env_rgn_functions_worker, args=(ENV, rgn, shared_dict_env_rgn_fn, stop_event, manager)
+            )
+            processes_get_fn.append(process)
+            process.start()
+        try:
+            for process in processes_get_fn:
+                process.join()
+            for k, v in shared_dict_env_rgn_fn.items():
+                rgn_fn_all[k] = dict(v)  # 将 manager.dict 转换为普通 dict
+        except KeyboardInterrupt as e:
+            stop_event.set()
+            for process in processes_get_fn:
+                process.join()
+            # 重新抛出异常
+            raise e
+
+    # 过滤出当前环境的函数
+    fn_curr_env_rgn: List[Function] = []
+    for _, fn_dict in rgn_fn_all.items():
+        print(f'Processing region: {_} {type(fn_dict)} {len(fn_dict)}')
+        for fn_name, fn_info in fn_dict.items():
+            is_env_main, is_env_sub = False, False
+            if not fn_name.startswith(ENV.name):
+                continue
+            if fn_name.endswith('FnStateChange'):
+                continue
+            if fn_name.startswith(f'{ENV.name}--{SUB_ENV}'):
+                # 子环境方法
+                is_env_sub = True
+            elif fn_name.replace(f'{ENV.name}-', '')[0] in string.ascii_uppercase:
+                # 主环境方法
+                is_env_main = True
+            # 不是当前环境的方法
+            if not (is_env_main or is_env_sub):
+                continue
+            fn_curr_env_rgn.append(Function.from_dict(fn_info))
+
+    # 获取所有地区的函数并发设置
+    with multiprocessing.Manager() as manager:
+        stop_event = multiprocessing.Event()
+        shared_dict_fn_ccy = manager.dict()
+        processes_func_ccy = []
+        for rgn, rgn_fn in rgn_fn_all.items():
+            fn_names = list(rgn_fn.keys())
+            for fn_name in fn_names:
+                process = multiprocessing.Process(
+                    target=get_functions_currency_worker, args=(ENV, rgn, fn_name, shared_dict_fn_ccy, stop_event)
+                )
+                processes_func_ccy.append(process)
+                process.start()
+        try:
+            for process in processes_func_ccy:
+                process.join()
+            fn_ccy_all = dict(shared_dict_fn_ccy)
+        except KeyboardInterrupt as e:
+            stop_event.set()
+            for process in processes_func_ccy:
+                process.join()
+            # 重新抛出异常
+            raise e
+
+    # 输出函数表格
+    for fn in fn_curr_env_rgn:
+        fn_rgn = fn.FunctionArn.split(':lambda:')[1].split(':')[0]
+        fn_ccy = fn_ccy_all.get(fn.FunctionName, {})
+        if 'ReservedConcurrentExecutions' in fn_ccy:
+            reserved_ccy = fn_ccy['ReservedConcurrentExecutions']
+            ccy_setting = 'Throttled' if reserved_ccy == 0 else f'{reserved_ccy}'
+        else:
+            ccy_setting = '非预留账户并发'
+        reserved_ccy = fn_ccy_all.get(fn.FunctionName, {}).get('ReservedConcurrentExecutions', -1)
+        last_modified_dt = datetime.strptime(fn.LastModified, '%Y-%m-%dT%H:%M:%S.%f%z') if fn.LastModified else None
+        table.insert_row(FunctionRow(
+            FunctionName=fn.FunctionName,
+            Region=fn_rgn,
+            Timeout=fn.Timeout,
+            MemorySize=fn.MemorySize,
+            CurrencySetting=ccy_setting,
+            LastDeployDt=datetime.strftime(last_modified_dt, '%Y-%m-%d %H:%M %z')[:-2] if last_modified_dt else 'NA',
+            FunctionName_href=get_lambda_url(fn_rgn, fn.FunctionName),
+        ))
+    table.print_table(order_by=['FunctionName', 'Region'])
+
+
 def main():
     fn_all: List[Function] = []
     fn_ccy_all: Dict = {}
     table = FunctionTable()
     for rgn in REGIONS:
         file_name_functions = f'{"prod" if ENV.is_prod_aws else "dev"}_{rgn}_Functions.json'
-
+        file_path_functions = __get_json_path(file_name_functions)
+        if os.path.exists(file_path_functions) and os.path.isfile(file_path_functions):
+            # 检查文件最后修改时间
+            print(datetime.fromtimestamp(os.path.getmtime(file_path_functions)))
+            # 读取本地文件
+            rgn_funcs = read_list_of_dict_from_json(file_name_functions)
         rgn_funcs = get_env_rgn_functions(env=ENV, region=rgn)
         save_list_of_dict_to_json(file_name_functions, rgn_funcs)
         rgn_funcs = read_list_of_dict_from_json(file_name_functions)
@@ -294,4 +448,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main_parallel()

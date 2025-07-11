1ca24d672e43b3db76d6e75ef19846754c6d2d3d
SamsonX <xialijie@sourcetech.xyz>
2025-07-11 17:40:45 +0800

    pref[get_lambda_info_async]: for each gateway region, fuse list_functions and get_function_concurrency step

--- Lambda/get_lambda_info_async.py
+++ Lambda/get_lambda_info_async.py
@@ -3,7 +3,7 @@ import os
 import string
 import sys
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from aiobotocore.session import AioSession
 from botocore.client import Config as BotoConfig
@@ -29,6 +29,7 @@ REGIONS = [
 ]
 # endregion 配置项
 
+DT_FMT = '%Y-%m-%d %H:%M:%S'
 _SESSION_CACHE: Dict[str, AioSession] = {}  # Cache for sessions
 
 
@@ -41,6 +42,18 @@ def get_cached_session(region: str, env: Env) -> AioSession:
 
 
 async def get_env_rgn_functions_async(env: Env, region: str, verbose: bool = False) -> Dict[str, dict]:
+    """_summary_
+
+    Args:
+        env (Env): environment
+        region (str): AWS region
+
+    Returns:
+        Dict[str, dict]: key为函数名，value为函数信息的字典
+    """
+    if verbose:
+        dt_start = datetime.now()
+        print(f'{dt_start.strftime(DT_FMT)} >> Get functions for region start. region={region}')
     session = get_cached_session(region=region, env=env)
     async with session.create_client(
         'lambda',
@@ -67,6 +80,12 @@ async def get_env_rgn_functions_async(env: Env, region: str, verbose: bool = Fal
             functions += resp.get('Functions', [])
             if not marker:
                 break
+        if verbose:
+            dt_stop = datetime.now()
+            print(
+                f'{dt_stop.strftime(DT_FMT)} >> Get functions for region stop. region={region}, '
+                f'span={(dt_stop - dt_start).total_seconds()}s'
+            )
         return {fn['FunctionName']: fn for fn in functions if 'FunctionName' in fn}
 
 
@@ -74,6 +93,13 @@ _RGN_SEMAPHORE: Dict[str, asyncio.Semaphore] = {}  # tune this based on your obs
 
 
 async def get_all_function_concurrency(env: Env, fn_list: List[Function]) -> Dict[str, dict]:
+    """获取所有函数的并发设置
+    Args:
+        env (Env): 环境配置
+        fn_list (List[Function]): 函数列表
+    Returns:
+        Dict[str, dict]: key为函数名，value为并发设置的字典
+    """
     concurrency_data = {}
 
     # Group by region
@@ -124,45 +150,47 @@ async def get_function_currency_async(env: Env, region: str, function_name: str)
             return {}
 
 
+async def region_function_coroutine(rgn: str) -> Dict[str, Tuple[Function, dict]]:
+    """获取地区内所有函数信息和并发设置
+
+    Args:
+        rgn (str): AWS region
+
+    Returns:
+        Dict[str, Tuple[Function, dict]]: key为函数名，value为函数对象、并发设置的元组
+    """
+    fn_dict: Dict[str, dict] = await get_env_rgn_functions_async(ENV, rgn)
+
+    fn_curr_env_rgn: List[Function] = []
+    for fn_name, fn_info in fn_dict.items():
+        is_env_main, is_env_sub = False, False
+        if not fn_name.startswith(ENV.name):
+            continue
+        if fn_name.endswith('FnStateChange'):
+            continue
+        if fn_name.startswith(f'{ENV.name}--{SUB_ENV}'):
+            is_env_sub = True
+        elif fn_name.replace(f'{ENV.name}-', '')[0] in string.ascii_uppercase:
+            is_env_main = True
+        if not (is_env_main or is_env_sub):
+            continue
+        fn_curr_env_rgn.append(Function.from_dict(fn_info))
+
+    fn_ccy = await get_all_function_concurrency(ENV, fn_curr_env_rgn)
+
+    ret = {fn.FunctionName: (fn, fn_ccy.get(fn.FunctionName, {})) for fn in fn_curr_env_rgn}
+    return ret
+
+
 async def main_coroutine():
-    fn_ccy_all: Dict = {}
     table = FunctionTable()
 
-    # 获取所有地区的函数信息
-    rgn_fn_all: Dict[str, Dict[str, dict]] = {}
-    tasks = [
-        get_env_rgn_functions_async(ENV, rgn, True)
-        for rgn in REGIONS
-    ]
-    results = await asyncio.gather(*tasks)
-    for rgn, fn_dict in zip(REGIONS, results):
-        rgn_fn_all[rgn] = fn_dict
+    results: List[Dict[str, Tuple[Function, dict]]]
+    results = await asyncio.gather(*(region_function_coroutine(rgn) for rgn in REGIONS))
+    fn_fn_ccy = [elem for result in results for elem in result.values()]  # Flatten the list of dicts
 
-    # 过滤出当前环境的函数
-    fn_curr_env_rgn: List[Function] = []
-    for _, fn_dict in rgn_fn_all.items():
-        print(f'Processing region: {_} {type(fn_dict)} {len(fn_dict)}')
-        for fn_name, fn_info in fn_dict.items():
-            is_env_main, is_env_sub = False, False
-            if not fn_name.startswith(ENV.name):
-                continue
-            if fn_name.endswith('FnStateChange'):
-                continue
-            if fn_name.startswith(f'{ENV.name}--{SUB_ENV}'):
-                is_env_sub = True
-            elif fn_name.replace(f'{ENV.name}-', '')[0] in string.ascii_uppercase:
-                is_env_main = True
-            if not (is_env_main or is_env_sub):
-                continue
-            fn_curr_env_rgn.append(Function.from_dict(fn_info))
-
-    # 获取所有地区的函数并发设置
-    fn_ccy_all = await get_all_function_concurrency(ENV, fn_curr_env_rgn)
-
-    # 输出函数表格
-    for fn in fn_curr_env_rgn:
+    for fn, fn_ccy in fn_fn_ccy:
         fn_rgn = fn.FunctionArn.split(':lambda:')[1].split(':')[0]
-        fn_ccy = fn_ccy_all.get(fn.FunctionName, {})
         if not fn_ccy:
             ccy_setting = '未知'
         elif 'ReservedConcurrentExecutions' in fn_ccy:
@@ -170,7 +198,7 @@ async def main_coroutine():
             ccy_setting = 'Throttled' if reserved_ccy == 0 else f'{reserved_ccy}'
         else:
             ccy_setting = '非预留账户并发'
-        reserved_ccy = fn_ccy_all.get(fn.FunctionName, {}).get('ReservedConcurrentExecutions', -1)
+        reserved_ccy = fn_ccy.get('ReservedConcurrentExecutions', -1)
         last_modified_dt = datetime.strptime(fn.LastModified, '%Y-%m-%dT%H:%M:%S.%f%z') if fn.LastModified else None
         table.insert_row(FunctionRow(
             FunctionName=fn.FunctionName,
@@ -188,5 +216,3 @@ if __name__ == '__main__':
     ret = asyncio.run(main_coroutine())
     if ret:
         print(f'Error: {ret}')
-    else:
-        print('Done.')
